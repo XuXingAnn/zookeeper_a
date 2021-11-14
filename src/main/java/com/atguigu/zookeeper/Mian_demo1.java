4904a890bc9175ca1281eb19f693e1d3f9d1cdfa
allen <1239972635@qq.com>
2021-11-14 15:47:11 +0800

    1.0 创建节点

---
+++ src/main/java/com/atguigu/zookeeper/Mian_demo1.java
@@ -0,0 +1,45 @@
+package com.atguigu.zookeeper;
+
+
+import org.apache.zookeeper.*;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class Mian_demo1 {
+
+
+    private ZooKeeper zooKeeper = null;
+
+
+//    连接 zookeeper
+    @Before
+    public void init() throws IOException {
+        String url= "192.168.1.19:2181";
+        int sessionTimeout = 200000;
+        zooKeeper = new ZooKeeper(url, sessionTimeout, new Watcher() {
+            @Override
+            public void process(WatchedEvent watchedEvent) {
+                System.out.println("Watcher");
+            }
+        });
+    }
+
+    @Test
+    public void  creatNode() throws KeeperException, InterruptedException {
+        // 参数 1：要创建的节点的路径； 参数 2：节点数据 ； 参数 3：节点权限 ；参数 4：节点的类型
+        final String s = zooKeeper.create("/java",
+                "allen".getBytes(StandardCharsets.UTF_8),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.PERSISTENT);
+        System.out.println(s);
+    }
+
+    @Test
+    public void getNode(){
+
+    }
+}
